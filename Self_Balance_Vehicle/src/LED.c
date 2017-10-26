55df8f7e3ef9bc810f55961c639402bee4241e89
kyChu <hello.kychu@gmail.com>
2017-10-26 11:42:50 +0800

    fix up annotation

--- Self_Balance_Vehicle/src/LED.c
+++ Self_Balance_Vehicle/src/LED.c
@@ -4,10 +4,10 @@ void LED_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	/* GPIOD Periph clock enable */
+	/* LED_GPIO Periph clock enable */
 	RCC_APB2PeriphClockCmd(LED_GPIO_PORT_CLK, ENABLE);
 
-	/* Configure PD0 and PD2 in output pushpull mode */
+	/* Configure LED_PIN in output pushpull mode */
 	GPIO_InitStructure.GPIO_Pin = LED_GPIO_PIN;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
