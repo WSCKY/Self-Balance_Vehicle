0c43347a935fba66c8dc8306cf7f2de0ff4f5bbd
kyChu <hello.kychu@gmail.com>
2017-10-29 13:51:38 +0800

    add the definition of button command confirm time && any includes

--- Self_Balance_Vehicle/inc/MainCtrl.h
+++ Self_Balance_Vehicle/inc/MainCtrl.h
@@ -1,9 +1,16 @@
 #ifndef __MAINCTRL_H
 #define __MAINCTRL_H
 
+#include "Button.h"
 #include "Encoder.h"
+#include "TB6612FNG.h"
+#include "pidController.h"
 #include "AttitudeEstimator.h"
+#include "AttitudeController.h"
 
 #define ABS(x) ((x) >= 0 ? (x) : -(x))
+
+#define RUN_ENABLE_CONFIRM             1000
+#define RUN_DISABLE_CONFIRM            100
 
 #endif /* __MAINCTRL_H */
