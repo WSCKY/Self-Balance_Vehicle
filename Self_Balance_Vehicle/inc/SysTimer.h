82cc63c3931c69abb9c240a17e09e3c0d633f86a
kyChu <hello.kychu@gmail.com>
2017-10-29 09:22:11 +0800

    change system timer to TIM3.(TIM2 & TIM4 are Used for the Encoders)

--- Self_Balance_Vehicle/inc/SysTimer.h
+++ Self_Balance_Vehicle/inc/SysTimer.h
@@ -3,11 +3,11 @@
 
 #include "stm32f10x_tim.h"
 
-#define SYS_TIMER            TIM2
-#define SYS_TIMER_CLK        RCC_APB1Periph_TIM2
+#define SYS_TIMER            TIM3
+#define SYS_TIMER_CLK        RCC_APB1Periph_TIM3
 
-#define SYS_TIMER_IRQn       TIM2_IRQn
-#define SYS_TIMER_Handler    TIM2_IRQHandler
+#define SYS_TIMER_IRQn       TIM3_IRQn
+#define SYS_TIMER_Handler    TIM3_IRQHandler
 
 void SysTimerInit(void);
 void SystemControlTask(void);
