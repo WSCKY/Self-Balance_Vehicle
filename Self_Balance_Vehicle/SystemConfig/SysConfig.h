b023131537dfa4bc1bf7fabd11ee1c8b931158c3
kyChu <hello.kychu@gmail.com>
2017-11-02 14:14:41 +0800

    define led flash rate && debug data send rate

--- Self_Balance_Vehicle/SystemConfig/SysConfig.h
+++ Self_Balance_Vehicle/SystemConfig/SysConfig.h
@@ -4,4 +4,10 @@
 /* system controller loop rate (in Hz) */
 #define SYSTEM_LOOP_RATE               (200)
 
+/* LED flash rate define */
+#define LED_FLASH_RATE_WAIT_STABLE     (10)
+#define LED_FLASH_RATE_SIGNAL_LOST     (2)
+
+#define DEBUG_DATA_FRAME_RATE          (100)
+
 #endif /* __SYSCONFIG_H */
