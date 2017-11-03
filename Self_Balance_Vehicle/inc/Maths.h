abd782cd396fe26eb45f44b83a8510284c006ab6
kyChu <hello.kychu@gmail.com>
2017-11-03 14:34:39 +0800

    add limit macros

--- Self_Balance_Vehicle/inc/Maths.h
+++ Self_Balance_Vehicle/inc/Maths.h
@@ -4,6 +4,10 @@
 #include <math.h>
 #include <stdint.h>
 
+#define LIMIT_MAX(x, y)                (((x) > (y)) ? (y) : (x))
+#define LIMIT_MIN(x, y)                (((x) < (y)) ? (y) : (x))
+#define LIMIT_RANGE(x, max, min)       (LIMIT_MAX(LIMIT_MIN((x), (min)), (max)))
+
 float apply_deadband(float value, float deadband);
 void step_change(float *in, float target, float step, float deadBand);
 
